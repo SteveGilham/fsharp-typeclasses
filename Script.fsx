b1e6299c9d88346dbc6ed24609d65ed299638b3e
gustavo perezleon <gustavo.perezleon@gmail.com>
2011-09-11 21:05:44 +0000

    + usage with a custom type (Tree)

--- Script.fsx
+++ Script.fsx
@@ -44,3 +44,22 @@ let lstTimes2   = fmap times2 [1;2;3;4]
 let times2plus3 = fmap times2 plus3
 let getChars    = fmap (fun (x:string) -> x.ToCharArray() |> Seq.toList ) action
 // try -> IO.Invoke getChars ;;
+
+
+
+
+// Define a type Tree
+
+type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Leaf of 'a
+    static member map f (t:Tree< 'a>  )  =
+        match t with
+        | Leaf x -> Leaf (f x)
+        | Tree(x,t1,t2) -> Tree(f x, Tree.map f t1,  Tree.map f t2)
+
+// add instance for Functor class
+    static member (?) (x:Tree<_>,cs:Fmap )   = fun f -> Tree.map    f x
+
+let myTree = Tree(6, Tree(2, Leaf(1), Leaf(3)), Leaf(9))
+let mappedTree = fmap times2plus3 myTree
