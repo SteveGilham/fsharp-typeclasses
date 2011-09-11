4987d387144b7909108a666d9197d1af01980de3
gustavo perezleon <gustavo.perezleon@gmail.com>
2011-09-11 17:45:52 +0000

    First version with functors, monads, IO and do notation.

---
+++ Prelude.fs
@@ -0,0 +1,61 @@
+﻿module Prelude
+
+
+// IO ---------------------------------------------------------------------
+
+type IO<'a> = IO of (unit->'a) with static member Invoke(IO(f)) : 'a = f()
+let primretIO  f    = IO(fun () -> f)
+let primbindIO io f = IO(fun () -> IO.Invoke (f (IO.Invoke io )))
+
+let getLine    = IO(fun() -> System.Console.ReadLine())
+let putStrLn x = IO(fun() -> printfn "%s" x)
+
+
+// Functor class ----------------------------------------------------------
+
+type Fmap() =
+    static member (?) (x:option<_>,cs:Fmap)  = fun f -> Option.map  f x
+    static member (?) (x:list<_>  ,cs:Fmap)  = fun f -> List.map    f x
+    static member (?) (x:IO<_>    ,cs:Fmap)  = fun f -> primbindIO x (primretIO << f)
+    static member (?) (g:_->_     ,cs:Fmap)  = fun f x -> f (g x)
+
+let inline fmap f x = (x ? (Fmap()) ) f
+
+
+// Monad class ------------------------------------------------------------
+
+type Return() =
+    static member (?<-) (notUsed:Return, cs:Return, t:'a option) = fun (x:'a) -> Some x
+    static member (?<-) (notUsed:Return, cs:Return, t:'a list)   = fun (x:'a) -> [x]
+    static member (?<-) (notUsed:Return, cs:Return, t:'a IO )    = fun (x:'a) -> primretIO x
+
+let inline return' x : 'R = ((Return()) ? ( Return()) <- Unchecked.defaultof< 'R> ) x
+
+type Bind() =
+    static member (?) (x:option<_>, cs:Bind) =
+        fun k ->
+            match x with
+            | Some v -> k v 
+            | None   -> None
+
+    static member (?) (x:list<_>  , cs:Bind) =
+        fun f ->
+            let rec bindForLists lst f =
+                match lst with
+                | x::xs -> f x @ (bindForLists xs f)
+                | [] -> [] 
+            bindForLists x f
+
+    static member (?) (x:IO<_>, cs:Bind)   = fun f -> primbindIO x f
+
+let inline (>>=) x = x ? (Bind())
+
+
+// Do notation ------------------------------------------------------------
+
+type DoNotationBuilder() =
+    member inline b.Return(x) = return' x
+    member inline b.Bind(p,rest) = p >>= rest
+    member b.Let(p,rest) = rest p
+
+let do' = new DoNotationBuilder()
---
+++ Script.fsx
@@ -0,0 +1,46 @@
+﻿#load "Prelude.fs"
+open Prelude
+
+
+// return
+
+let anOption :option<_> = return' 2
+let aList    :list<_>   = return' 2
+
+
+// List Monad
+
+// F#                           // Haskell
+let result = 
+    do' {                       // do {
+        let! x1 = [1;2]         //   x1 <- [1;2]
+        let! x2 = [10;20]       //   x2 <- [10;20]
+        return ((+) x1 x2) }    //   return (f x1 x2) }
+
+// desugared version
+let binded = [1;2]  >>= (fun x1 -> [10;20] >>= (fun x2 ->  return'((+) x1 x2 )))
+
+// IO Monad
+
+let action = do' {
+    let! _  = putStrLn  "What is your first name?"
+    let! fn = getLine
+    let! _  = putStrLn  ("Thanks, " + fn) 
+    let! _  = putStrLn  ("What is your last name?")
+    let! ln = getLine
+    let  fullname = fn + " " + ln
+    let! _  = putStrLn  ("Your full name is: " + fullname)
+    return fullname }
+// try -> IO.Invoke action ;;
+
+
+// Functors
+
+let times2,plus3 = (*) 2, (+) 3
+
+let valTimes3   = fmap plus3 (Some 4)
+let noValue     = fmap plus3 None
+let lstTimes2   = fmap times2 [1;2;3;4]
+let times2plus3 = fmap times2 plus3
+let getChars    = fmap (fun (x:string) -> x.ToCharArray() |> Seq.toList ) action
+// try -> IO.Invoke getChars ;;
