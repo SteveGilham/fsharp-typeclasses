37404eac113c3ca3d0668b511d7d136f8f30f04e
gustavo perezleon <gustavo.perezleon@gmail.com>
2011-09-15 23:40:05 +0000

    Abbreviated. Applicative (->) re-written, based on the monad.

--- Applicative.fs
+++ Applicative.fs
@@ -9,7 +9,7 @@ type Apply = Apply with
     static member (?<-) (f:option<_> ,cs:Apply ,x:option<_> ) = ap f x
     static member (?<-) (f:list<_>   ,cs:Apply ,x:list<_>)    = ap f x
     static member (?<-) (f:IO<_>     ,cs:Apply ,x )           = ap f x
-    static member (?<-) (f:_ -> _    ,cs:Apply ,g: _ -> _ )   = fun x -> (f x) (g x)
+    static member (?<-) (f:_ -> _    ,cs:Apply ,g: _ -> _ )   = ap f g
 
 let inline (<*>) x y = x ? (Apply) <- y
 
@@ -20,6 +20,6 @@ let inline (<*  )   x   = x |> liftA2 const'
 let inline (<**>)   x   = x |> liftA2 (|>)
 
 type ZipList<'a> = ZipList of 'a seq with
-    static member (?)   (ZipList(x)    , cs: Fmap)                  = fun f -> ZipList (Seq.map f x)
-    static member (?<-) (notUsed:Return, cs:Return, t: ZipList<'a>) = fun x -> ZipList (Seq.initInfinite (fun _ -> x))
-    static member (?<-) (ZipList(f)    , cs:Apply , ZipList(x))     = ZipList (Seq.zip f x |> Seq.map (fun (f,x) -> f x))
+    static member (?)   (ZipList x, cs: Fmap)                 = fun f -> ZipList (Seq.map f x)
+    static member (?<-) (_:Return , cs:Return, t:ZipList<'a>) = fun x -> ZipList (Seq.initInfinite (fun _ -> x))
+    static member (?<-) (ZipList f, cs:Apply , ZipList x)     = ZipList (Seq.zip f x |> Seq.map (fun (f,x) -> f x))
