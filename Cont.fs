7aee680927541ae91fd16fccf4c179330d89eef4
gustavo perezleon <gustavo.perezleon@gmail.com>
2011-10-20 20:20:30 +0000

    + Continuation Monad

---
+++ Cont.fs
@@ -0,0 +1,11 @@
+﻿module Control.Monad.Cont
+
+open Prelude
+type Cont<'r,'a> = Cont of (('a->'r)->'r) with
+    static member (?<-) (_:Return, cs:Return, t:Cont<_,'a>) = fun (n:'a) -> Cont (fun k -> k n)
+    static member (?) (m:Cont<_,_>, cs:Bind) =
+        let runCont (Cont(x)) = x
+        fun f -> Cont (fun k -> runCont m (fun a -> runCont (f a) k))
+
+let runCont (Cont(x)) = x
+
