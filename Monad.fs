234817e49c37b9d15219ef10c9f719a7aca4b3ea
gustavo perezleon <gustavo.perezleon@gmail.com>
2011-10-21 06:52:41 +0000

    + when and unless functions.

--- Monad.fs
+++ Monad.fs
@@ -2,6 +2,8 @@
 
 open Prelude
 
-let inline liftM2 f m1 m2 = do' { let! x1 = m1 in let! x2 = m2 in return (f x1 x2) }
-let inline ap x = liftM2 id x
+let inline liftM2 f m1 m2   = do' { let! x1 = m1 in let! x2 = m2 in return (f x1 x2) }
+let inline when' p s        = if p then s else return' ()
+let inline unless p s       = when' (not p) s
+let inline ap x             = liftM2 id x
 
